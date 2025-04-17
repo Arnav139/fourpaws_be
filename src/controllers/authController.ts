97051de570fe933fad087cef0c135a6aa14cab4f
Vimal Saraswat <vimal@thewasserstoff.com>
2025-04-17 12:08:04 +0530

    Fix generateAuthTokens from async to sync call

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,45 +1,49 @@
 import { Request, Response } from "express";
 import redisClient from "../config/redis";
-import {UserService} from "../services";
+import { UserService } from "../services";
 import Mailer from "../utils/nodeMailer";
 import { generateAuthTokens } from "../config/token";
 import { otpToken } from "../config/common";
 
-
-export default class authController{
-  static verifyOtp:any = async(req:Request,res:Response)=>{
+export default class authController {
+  static verifyOtp: any = async (req: Request, res: Response) => {
     try {
-      const {otp} = req.body;
-      console.log(req['user'],"in controller");
-      let userExists = await Mailer.verifyOtp(req['user'] as any,otp);
-        if (!userExists) {
-          userExists = await UserService.insertUser(req['user']['email'] as string);
-        }
-      const accessToken = await generateAuthTokens(userExists.id);
-      const user = {...userExists,accessToken}
-      return res.status(200).send({success: true,message:"user logged  in",user});
+      const { otp } = req.body;
+      console.log(req["user"], "in controller");
+      let userExists = await Mailer.verifyOtp(req["user"] as any, otp);
+      if (!userExists) {
+        userExists = await UserService.insertUser(
+          req["user"]["email"] as string,
+        );
+      }
+      const accessToken = generateAuthTokens(userExists.id);
+      const user = { ...userExists, accessToken };
+      return res
+        .status(200)
+        .send({ success: true, message: "user logged  in", user });
     } catch (error) {
-      console.log(error)
-      return res.status(500).send({success:false,error:"invalid otp"});
+      console.log(error);
+      return res.status(500).send({ success: false, error: "invalid otp" });
     }
-  }
-  
+  };
+
   // post /auth/login
-static loginUser = async (req: Request, res: any) => {
+  static loginUser = async (req: Request, res: any) => {
     const { email } = req.body;
-  
+
     if (!email) {
       return res.status(400).json({
         success: false,
         error: "Email and password are required",
       });
     }
-  
+
     try {
-      const otp= await Mailer.sendEmail(email)
-      let token = otpToken(email,otp);
-      return res.status(200).send({success:true,message:"OTP Sent SuccessFully",token})
-  
+      const otp = await Mailer.sendEmail(email);
+      let token = otpToken(email, otp);
+      return res
+        .status(200)
+        .send({ success: true, message: "OTP Sent SuccessFully", token });
     } catch (error) {
       console.error("Error in loginUser:", error);
       return res.status(500).json({
@@ -48,18 +52,18 @@ static loginUser = async (req: Request, res: any) => {
       });
     }
   };
-  
+
   // POST /auth/update-wallet
-static updateWallet = async (req: Request, res: any) => {
+  static updateWallet = async (req: Request, res: any) => {
     const { email, walletAddress } = req.body;
-  
+
     if (!email || !walletAddress) {
       return res.status(400).json({
         success: false,
         error: "Email and wallet address are required",
       });
     }
-  
+
     try {
       // Check if the user exists
       const userExists = await UserService.getUser(email);
@@ -69,22 +73,21 @@ static updateWallet = async (req: Request, res: any) => {
           error: "User does not exist, please register first",
         });
       }
-  
+
       // Check if wallet address is already set
-      const currentWalletAddress = await UserService.getWalletAddressByEmail(
-        email
-      );
+      const currentWalletAddress =
+        await UserService.getWalletAddressByEmail(email);
       if (currentWalletAddress) {
         return res.status(400).json({
           success: false,
           error: "Wallet address already set",
         });
       }
-  
+
       // Update the wallet address
       const updatedUser = await UserService.updateWalletAddress(
         email,
-        walletAddress
+        walletAddress,
       );
       return res.status(200).json({
         success: true,
@@ -100,5 +103,3 @@ static updateWallet = async (req: Request, res: any) => {
     }
   };
 }
-
-
